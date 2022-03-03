702077bba71d70d844376c6c7b92bc962ea10f1c
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-03 16:15:33 +0100

    Support for Safari 15
    Theme provider
    add environment variables

--- src/components/TestComponent/TestComponent.jsx
+++ src/components/TestComponent/TestComponent.jsx
@@ -22,6 +22,8 @@ function TestComponent(props) {
             <h3>Dette er en h3</h3>
             <h4>Dette er en h4</h4>
             <h5>Dette er en h5</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit condimentum nibh ac vehicula. Mauris id nulla vel eros aliquam mollis. Phasellus a commodo diam. Ut luctus tellus vel lacus aliquet iaculis. Curabitur in lobortis nulla. Suspendisse efficitur arcu eget turpis aliquam, nec facilisis sapien consectetur. Nulla convallis consectetur justo vitae auctor. Morbi id lacus imperdiet, aliquam orci non, fermentum velit. Nullam nibh nibh, gravida et euismod a, semper sed lorem. Aliquam consequat at nibh tristique ornare. Morbi mollis accumsan tortor, tempor rhoncus sapien rutrum et. Cras eleifend suscipit varius. Aliquam ac rhoncus elit. Donec ullamcorper erat nec imperdiet sodales. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit condimentum nibh ac vehicula. Mauris id nulla vel eros aliquam mollis. Phasellus a commodo diam. Ut luctus tellus vel lacus aliquet iaculis. Curabitur in lobortis nulla. Suspendisse efficitur arcu eget turpis aliquam, nec facilisis sapien consectetur. Nulla convallis consectetur justo vitae auctor. Morbi id lacus imperdiet, aliquam orci non, fermentum velit. Nullam nibh nibh, gravida et euismod a, semper sed lorem. Aliquam consequat at nibh tristique ornare. Morbi mollis accumsan tortor, tempor rhoncus sapien rutrum et. Cras eleifend suscipit varius. Aliquam ac rhoncus elit. Donec ullamcorper erat nec imperdiet sodales. </p>
         </>
     );
 }
