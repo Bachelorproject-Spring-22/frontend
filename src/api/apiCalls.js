8152d8040c276a7b04133105b1a9fb9fc3ef0fe3
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-03 14:29:09 +0100

    *finished now

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,15 +1,9 @@
 import axios from './axios';
-import pureAxios from 'axios';
-
-const onAppLoad = () => {
-    return pureAxios.post('/api/refresh');
-}
 
 const getter = () => {
     return axios.get('/');
 }
 
 export {
-    onAppLoad,
     getter
 };
