ed122f41970248496f1af4fc492fbe1339d2b2e1
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-07 19:28:47 +0100

    Added popup, not finished

--- src/components/Settings/Account/Account.jsx
+++ src/components/Settings/Account/Account.jsx
@@ -1,30 +1,40 @@
+import { useState } from "react";
 import Button from "../../Button/Button";
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import Popup from "../../PopUp/PopUp";
 
 function Account(props) {
     const data = props.data;
+    const [popUp, setPopUp] = useState(false);
+
+    const handleClick = () => {
+        setPopUp(prevState => !prevState);
+    }
 
     return (
-        <section className='account-settings'>
-            <h1>Account</h1>
-            <p className="subtitle">{data.user.username}</p>
+        <>
+        {popUp ? <Popup content={<p>Popup</p>} /> : null}
+            <section className='account-settings'>
+                <h1>Account</h1>
+                <p className="subtitle">{data.user.username}</p>
 
-            <article>
-                <div>
-                    <p>Email</p>
-                    <p className="user-text">Edit</p>
-                </div>
-                <div>
-                    <p>Password</p>
-                    <p className="user-text">Edit</p>
-                </div>
-                <div>
-                    <p>Cookie Consent</p>
-                    <p className="user-text">Edit</p>
-                </div>
-            </article>
-            <Button label='Log Out' icon={<LogoutRoundedIcon />} variant='secondary destructive' onClick={data.logoutUser} />
-        </section>
+                <article>
+                    <div onClick={handleClick}>
+                        <p>Email</p>
+                        <p className="user-text">Edit</p>
+                    </div>
+                    <div>
+                        <p>Password</p>
+                        <p className="user-text">Edit</p>
+                    </div>
+                    <div>
+                        <p>Cookie Consent</p>
+                        <p className="user-text">Edit</p>
+                    </div>
+                </article>
+                <Button label='Log Out' icon={<LogoutRoundedIcon />} variant='secondary destructive' onClick={data.logoutUser} />
+            </section>
+        </>
     );
 }
 
