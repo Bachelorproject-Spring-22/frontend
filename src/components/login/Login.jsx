130dad15285cbb565eea02a6052f2e6e208f174e
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-07 15:29:39 +0100

    add Helmet to about pages

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -57,9 +57,9 @@ class Login extends Component {
     render() {
         return (
             <>
-             <Helmet>
-                <title>Leaderboard - The Kahoot! League</title>
-            </Helmet>
+                <Helmet>
+                    <title>Log in - The Kahoot! League</title>
+                </Helmet>
                 <h1>The Kahoot! League</h1>
                 <p className='subtitle login'>LOG IN</p>
                 <p className='login'>If you have been invited to The Kahoot! League, you can enter your provided username and password below.</p>
