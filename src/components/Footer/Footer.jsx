c81379ca1b4ac8dd7bcf941ca9ab5583bae77ba4
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-09 09:18:10 +0100

    footer scales correctly

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,38 +9,40 @@ function Footer() {
     const scroll = () => {
         window.scroll(0, 0);
     }
-    
+
     return (
         <footer>
-            <h3>The Kahoot! League</h3>
-            <section>
-                <article>
-                    <p>Learn more</p>
-                    <Link to='/about'>About The Kahoot! League</Link>
-                    <Link to='/help'>Help</Link>
-                    <Link to='/contact'>Contact</Link>
-                    <Link to='/about/terms-of-service'>Terms of service</Link>
-                    <Link to='/about/privacy-policy'>Privacy policy</Link>
-                    <Link to='/about/cookie-policy'>Cookie policy</Link>
-                </article>
-                <article>
-                    <p>Want to Contribute?</p>
-                    <Link to='/developer'>Developer</Link>
-                </article>
-            </section>
-            <section className="created">
-                <p className="middle-emphasis">Created by Cornelius, Glenn and Tom.</p>
-                <p className="middle-emphasis">NTNU-Gjøvik, 2022</p>
-            </section>
-            <section className="logo-arrow">
-                <img src={NtnuLogo} alt="ntnu-logo" className="footer-logo" />
-                <Button onClick={scroll} label={""} icon={<ArrowUpwardRoundedIcon/>} variant={'fab'} ariaLabel={"Scroll to top"}/>
-            </section>
-            <section>
-                <div id="wcb" className="carbonbadge wcb-d"></div>
-                <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
-                <img src="https://api.thegreenwebfoundation.org/greencheckimage/www.ntnu.no" alt="This website is hosted Green - checked by thegreenwebfoundation.org" />
-            </section>
+            <div>
+                <h3>The Kahoot! League</h3>
+                <section>
+                    <article>
+                        <p>Learn more</p>
+                        <Link to='/about'>About The Kahoot! League</Link>
+                        <Link to='/help'>Help</Link>
+                        <Link to='/contact'>Contact</Link>
+                        <Link to='/about/terms-of-service'>Terms of service</Link>
+                        <Link to='/about/privacy-policy'>Privacy policy</Link>
+                        <Link to='/about/cookie-policy'>Cookie policy</Link>
+                    </article>
+                    <article>
+                        <p>Want to Contribute?</p>
+                        <Link to='/developer'>Developer</Link>
+                    </article>
+                </section>
+                <section className="created">
+                    <p className="middle-emphasis">Created by Cornelius, Glenn and Tom.</p>
+                    <p className="middle-emphasis">NTNU-Gjøvik, 2022</p>
+                </section>
+                <section className="logo-arrow">
+                    <img src={NtnuLogo} alt="ntnu-logo" className="footer-logo" />
+                    <Button onClick={scroll} label={""} icon={<ArrowUpwardRoundedIcon />} variant={'fab'} ariaLabel={"Scroll to top"} />
+                </section>
+                <section>
+                    <div id="wcb" className="carbonbadge wcb-d"></div>
+                    <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
+                    <img src="https://api.thegreenwebfoundation.org/greencheckimage/www.ntnu.no" alt="This website is hosted Green - checked by thegreenwebfoundation.org" />
+                </section>
+            </div>
         </footer>
     );
 }
