24b568cb1d203505e66393dd61b1933c5c106a54
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-09 09:03:19 +0100

    card + myresults (WIP)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,32 +1,29 @@
 import './card.css';
+import { NavLink } from 'react-router-dom';
 
 /* Denne må endres når vi vet hvordan payload-en fra back-end ser ut. Komponentet er dermed ikke avansert at all */
 
 function Card(props) {
     return (
-        <>
+        <li >
+            <NavLink className={"card"} to="/test">
             {
                 props.type === 'course' ? (
-
-                    <div className={`card ${props.type}`.trim()}>
                         <div>
                             <h3>{props.courseCode} – {props.fullCourseName} </h3>
                             <p>Placement: {props.placementRank}</p>
                         </div>
-                    </div>
-
                 ) : (
 
-                    <div className={`card ${props.type}`.trim()}>
                         <div>
                             <h3>Quiz {props.quizNumber} </h3>
                             <p>Correct answers: {props.correctAnswers}</p>
                             <p>Incorrect answers: {props.incorrectAnswers}</p>
                         </div>
-                    </div>
                 )
             }
-        </>
+            </NavLink>
+        </li>
     );
 }
 
--- src/components/MyResults/MyResults.jsx
+++ src/components/MyResults/MyResults.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Card from '../Card/Card';
 import './my-results.css';
 
+
 class MyResults extends Component {
     constructor(props) {
         super(props);
@@ -11,7 +12,7 @@ class MyResults extends Component {
         return (
             <>
                 <h1>My Results</h1>
-                
+
                 <h2>Aggregated leaderboards</h2>
                 <label htmlFor="leaderboard-type">Select a leaderboard:</label>
 
@@ -20,27 +21,37 @@ class MyResults extends Component {
                     <option value="academic">Academic year leaderboard</option>
                     <option value="study">Studyprogram leaderboard</option>
                 </select>
-                
+
                 <h2>Current semester</h2>
-                
-                <div className='my-results grid-container'>
-                    <Card type='course' courseCode='IDG1200' fullCourseName='Cloud Technologies' placementRank={3}/>
-                    <Card type='course' courseCode='IDG1200' fullCourseName='Information Structures and Database Systems' placementRank={3}/>
-                    <Card type='course' courseCode='IDG1200' fullCourseName='Full-stack development' placementRank={3}/>
-                    <Card type='course' courseCode='IDG1200' fullCourseName='Service Design ' placementRank={3}/>
-                </div>
-                
+                <ul className='my-results grid-container'>
+                    {/* ↓Her trenger vi en map som looper gjennom alle emnene studenten har nåværende semester. ↓ */}
+
+                    
+                        <Card type='course' courseCode='IDG1200' fullCourseName='Cloud Technologies' placementRank={3} />
+                    
+                    
+                        <Card type='course' courseCode='IDG1200' fullCourseName='sdfsdfsdfsdfsdfsdfsdfsdfCloud Technologies' placementRank={3} />
+                    
+                    
+                        <Card type='course' courseCode='IDG1200' fullCourseName='Cloud Technologies' placementRank={3} />
+                   
+                    
+                        <Card type='course' courseCode='IDG1200' fullCourseName='Cloud Technologies' placementRank={3} />
+                    
+
+                </ul>
+                <hr />
+
                 <h2>Previous semester</h2>
-                <h3>Semestertekst</h3>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <h3>Semestertekst</h3>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
+                {/* ↓ Her trenger vi en map som looper gjennom alle tidligere emner studenten har tatt før nåværende semester ↓*/}
+
+                <h3>Semestertekst</h3> {/* For eksempel: "Spring 2021" */}
+                <div className='my-results grid-container'>
+                    <Card type='course' courseCode='IDG1200' fullCourseName='Cloud Technologies' placementRank={3} />
+                    <Card type='course' courseCode='IDG1200' fullCourseName='Information Structures and Database Systems' placementRank={3} />
+                    <Card type='course' courseCode='IDG1200' fullCourseName='Full-stack development' placementRank={3} />
+                    <Card type='course' courseCode='IDG1200' fullCourseName='Service Design ' placementRank={3} />
+                </div>
             </>
         );
     }
