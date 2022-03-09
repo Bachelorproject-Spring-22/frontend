2aa2dc9ac53881426c0eb560e5e5f3b2fa3f5959
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-09 20:24:05 +0100

    Leaderboard complete

--- src/components/Leaderboard/IndividualLeaderboard/IndividualLeaderboard.jsx
+++ src/components/Leaderboard/IndividualLeaderboard/IndividualLeaderboard.jsx
@@ -1,7 +1,16 @@
+import React, { useMemo } from 'react';
+import makeData from '../../Table/makeData';
+import Table from "../../Table/Table";
+
 function IndividualLeaderboard(props) {
+    const data = useMemo(() => makeData(30), []);
+
     return (
         <section className='inidividual-leaderboard'>
-
+            <h1>Full-Stack Web Development</h1>
+            <p className='subtitle'>Leaderboard</p>
+            <p className='middle-emphasis'>The leaderboard display the top students from the last 8 quizes.</p>
+            <Table data={data} />
         </section>
     );
 };
