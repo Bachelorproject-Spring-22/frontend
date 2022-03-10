8d3f661720f252a3221ff8e6bd622b2cfc74c453
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-10 14:59:38 +0100

    add bar chart with gradient

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,9 +3,9 @@
 /* https://stackoverflow.com/a/70593795/14447555 -> https://www.chartjs.org/docs/3.3.0/getting-started/integration.html#bundlers-webpack-rollup-etc*/
 
 import { Bar } from 'react-chartjs-2';
-
+import { useRef, useEffect, useState } from 'react';
 import {
-    Chart,
+    Chart as ChartJS,
     CategoryScale,
     LinearScale,
     BarElement,
@@ -14,7 +14,7 @@ import {
     Legend
 } from 'chart.js';
 
-Chart.register(
+ChartJS.register(
     BarElement,
     CategoryScale,
     LinearScale,
@@ -23,17 +23,88 @@ Chart.register(
     Legend
 );
 
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+            labels: {
+                color: 'rgba(255, 255, 255, 0.87)'
+            }
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Bar Chart',
+            color: '#fff'
+        },
+    },
+    scales: {
+        y: {
+            ticks: {
+                color: 'rgba(255, 255, 255, 0.87)'
+            }
+        },
+        x: {
+            ticks: {
+                color: 'rgba(255, 255, 255, 0.87)'
+            }
+        }
+    }
+};
+
+
+const labels = ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4', 'Quiz 5', 'Quiz 6']
+
+const data = {
+    labels,
+    datasets: [{
+        label: 'Points',
+        data: [120, 190, 300, 500, 560, 200],
+        backgroundColor: 'rgb(0, 99, 132)'
+    }]
+}
+
+function createGradient(ctx, area) {
+    const colorStart = "#7F619C";
+    const colorEnd = "#BEAFD0";
+
+    const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+    gradient.addColorStop(0, colorStart);
+    gradient.addColorStop(1, colorEnd);
+
+    return gradient;
+}
+
 const BarChart = () => {
+    const [chartData, setChartData] = useState({
+        datasets: []
+      });
+
+    const chartRef = useRef(Bar);
+
+    useEffect(() => {
+        const chart = chartRef.current;
+
+        if (!chart) {
+            return;
+        }
+
+
+        const chartData = {
+            ...data,
+            datasets: data.datasets.map((dataset) => ({
+                ...dataset,
+                backgroundColor: createGradient(chart.ctx, chart.chartArea)
+            }))
+        };
+
+        setChartData(chartData);
+    },[]);
+
 
     return (
-        <Bar data={{
-            labels: ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4', 'Quiz 5', 'Quiz 6'],
-            datasets: [{
-                label: 'Points pr quiz',
-                data: [120, 190, 300, 500, 2000, 3000],
-                backgroundColor: 'rgb(255, 99, 132)'
-            }]
-        }} />
+        <Bar ref={chartRef} options={options} data={chartData} />
     );
 
 };
