d59d15bf649eb14e66154af05eb474631daf5dda
Tom Schrier <tomschr@stud.ntnu.no>
2022-03-12 18:47:32 +0100

    Better hover indication

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,39 +1,42 @@
 import './card.css';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 /* Denne må endres når vi vet hvordan payload-en fra back-end ser ut. Komponentet er dermed ikke avansert at all */
 
 function Card({ link = '/test', type, courseCode, fullCourseName, placementRank, quizNumber, correctAnswers, incorrectAnswers, number, label }) {
     return (
         <li>
-            <Link className="card" to={link}>
-                {
-                    type === 'course' ? (
+            {
+                type === 'course' ? (
+                    <Link className="card" to={link}>
                         <div>
                             <h3>{courseCode} – {fullCourseName} </h3>
                             <p>Placement: {placementRank}</p>
                         </div>
-                    ) : type === 'quiz' ? (
+                    </Link>
+                ) : type === 'quiz' ? (
 
+                    <Link className="card" to={link}>
                         <div>
                             <h3>Quiz {quizNumber} </h3>
                             <p>Correct answers: {correctAnswers}</p>
                             <p>Incorrect answers: {incorrectAnswers}</p>
                         </div>
-                    ) : type === 'individual' ? (
-                        <div>
-                            <h3>{number}</h3>
-                            <p className='subtitle'>{label}</p>
-                        </div>
-                    ) : (
-                        <div>
-                            <h3>Wrong type</h3>
-                        </div>
-                    )
-                }
-            </Link>
+                    </Link>
+                ) : type === 'individual' ? (
+                    <div className='card individual'>
+                        <h3>{number.toLocaleString()}</h3>
+                        {label ? <p className='subtitle'>{label}</p> : <p>–</p>}
+                    </div>
+                ) : (null)
+            }
         </li>
     );
+}
+
+Card.propTypes = {
+    type: PropTypes.oneOf(['course', 'quiz', 'individual']),
 }
 
 export default Card;
