2afced2616b47e2b43f2435607028aeea1f1c373
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-14 13:13:05 +0100

    Changed components to functions

--- src/components/MyResults/MyResults.jsx
+++ src/components/MyResults/MyResults.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import IndividualQuizResults from './IndividualQuizResults/IndividualQuizResults';
 import IndividualResults from './IndividualResults/IndividualResults';
@@ -6,30 +6,23 @@ import MainResults from './MainResults/MainResults';
 import './my-results.css';
 
 
-class MyResults extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <>
-                <nav>
-                    <Link to="/home" className='breadcrumbItem'>Home</Link>
-                    <Routes>
-                        <Route exact path='/idg2100' element={<>&#8250; <Link to="/home/idg2100" className='breadcrumbItem' >IDG2100</Link></>} />
-                        <Route exact path='/idg2100/quiz1' element={<>&#8250; <Link to="/home/idg2100" className='breadcrumbItem' >IDG2100</Link> &#8250; <Link to="/home/idg2100/quiz1" className='breadcrumbItem' >Quiz 1</Link></>} />
-                    </Routes>
-                </nav>
+function MyResults(props) {
+    return (
+        <>
+            <nav>
+                <Link to="/home" className='breadcrumbItem'>Home</Link>
                 <Routes>
-                    <Route exact path='/' element={<MainResults />} />
-                    <Route exact path='/idg2100' element={<IndividualResults />} />
-                    <Route exact path='/idg2100/quiz1' element={<IndividualQuizResults />} />
+                    <Route exact path='/idg2100' element={<>&#8250; <Link to="/home/idg2100" className='breadcrumbItem' >IDG2100</Link></>} />
+                    <Route exact path='/idg2100/quiz1' element={<>&#8250; <Link to="/home/idg2100" className='breadcrumbItem' >IDG2100</Link> &#8250; <Link to="/home/idg2100/quiz1" className='breadcrumbItem' >Quiz 1</Link></>} />
                 </Routes>
-            </>
-        );
-    }
+            </nav>
+            <Routes>
+                <Route exact path='/' element={<MainResults />} />
+                <Route exact path='/idg2100' element={<IndividualResults />} />
+                <Route exact path='/idg2100/quiz1' element={<IndividualQuizResults />} />
+            </Routes>
+        </>
+    );
 }
 
 export default MyResults;
