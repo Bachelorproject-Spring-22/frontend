2b397ec4a434718d05742981470f92239c1fc2df
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-14 13:02:58 +0100

    Add alert popup

--- src/components/HOC/ManageHOC.jsx
+++ src/components/HOC/ManageHOC.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import AuthContext from '../../helpers/AuthContext';
 import PopUp from '../PopUp/PopUp';
 import Upload from '../TestComponent/Upload';
-import Confirm from '../PopUp/Confirm/Confirm'
+import Confirm from '../PopUp/Confirm/Confirm';
+import Alert from '../PopUp/Alert/Alert';
 
 function managePageBackend(WrappedComponent) {
     class ManageHOC extends Component {
@@ -29,7 +30,7 @@ function managePageBackend(WrappedComponent) {
             return (
                 <>
                     <WrappedComponent handleOpen={this.togglePop} {...this.props} />
-                    {this.state.uploadPop && <PopUp type='uploadPop' content={<Confirm modalTitle={'hello'} bodyText={'world'}/>} handleClose={this.togglePop} />}
+                    {this.state.uploadPop && <PopUp type='uploadPop' content={<Alert modalTitle={'hello'} bodyText={'world'}/>} handleClose={this.togglePop} />}
                     {this.state.addStudent && <PopUp type='addStudent' content={<p>Student</p>} handleClose={this.togglePop} />}
                     {this.state.addTeacher && this.context.user.role === 'superAdmin' && <PopUp type='addTeacher' content={<Upload />} handleClose={this.togglePop} />}
                 </>
