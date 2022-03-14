b2a03efb07fd16c39881d82ded52651e8efaf8fc
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-14 13:16:20 +0100

    add prompt modal

--- src/components/HOC/ManageHOC.jsx
+++ src/components/HOC/ManageHOC.jsx
@@ -4,6 +4,7 @@ import PopUp from '../PopUp/PopUp';
 import Upload from '../TestComponent/Upload';
 import Confirm from '../PopUp/Confirm/Confirm';
 import Alert from '../PopUp/Alert/Alert';
+import Prompt from '../PopUp/Prompt/Prompt';
 
 function managePageBackend(WrappedComponent) {
     class ManageHOC extends Component {
@@ -30,7 +31,7 @@ function managePageBackend(WrappedComponent) {
             return (
                 <>
                     <WrappedComponent handleOpen={this.togglePop} {...this.props} />
-                    {this.state.uploadPop && <PopUp type='uploadPop' content={<Alert modalTitle={'hello'} bodyText={'world'}/>} handleClose={this.togglePop} />}
+                    {this.state.uploadPop && <PopUp type='uploadPop' content={<Prompt modalTitle={'hello'} bodyText={'world'} labelText='this is a label'/>} handleClose={this.togglePop} />}
                     {this.state.addStudent && <PopUp type='addStudent' content={<p>Student</p>} handleClose={this.togglePop} />}
                     {this.state.addTeacher && this.context.user.role === 'superAdmin' && <PopUp type='addTeacher' content={<Upload />} handleClose={this.togglePop} />}
                 </>
