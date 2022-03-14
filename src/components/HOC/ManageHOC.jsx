083b22438394e113cba83c4f418a8c9d1cafbb89
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-14 12:59:54 +0100

    add confirm modal

--- src/components/HOC/ManageHOC.jsx
+++ src/components/HOC/ManageHOC.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AuthContext from '../../helpers/AuthContext';
 import PopUp from '../PopUp/PopUp';
 import Upload from '../TestComponent/Upload';
+import Confirm from '../PopUp/Confirm/Confirm'
 
 function managePageBackend(WrappedComponent) {
     class ManageHOC extends Component {
@@ -28,7 +29,7 @@ function managePageBackend(WrappedComponent) {
             return (
                 <>
                     <WrappedComponent handleOpen={this.togglePop} {...this.props} />
-                    {this.state.uploadPop && <PopUp type='uploadPop' content={<p>Upload</p>} handleClose={this.togglePop} />}
+                    {this.state.uploadPop && <PopUp type='uploadPop' content={<Confirm modalTitle={'hello'} bodyText={'world'}/>} handleClose={this.togglePop} />}
                     {this.state.addStudent && <PopUp type='addStudent' content={<p>Student</p>} handleClose={this.togglePop} />}
                     {this.state.addTeacher && this.context.user.role === 'superAdmin' && <PopUp type='addTeacher' content={<Upload />} handleClose={this.togglePop} />}
                 </>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
-import Phone from '../../assets/phone.avif';
-import Sky from '../../assets/sky.jpg';
+/* import Phone from '../../assets/phone.avif';
+import Sky from '../../assets/sky.jpg'; */
 import './Home.css';
 import SEO from '../SEO/SEO';
 import Icon from '../Icon/Icon';
@@ -22,7 +22,7 @@ function Home(props) {
         <section className='home-parent'>
             <article id='introduction-1'>
 
-                <img id="image1" src={Phone} alt="phone" loading="lazy" />
+                {/* <img id="image1" src={Phone} alt="phone" loading="lazy" /> */}
                 <div id="text1">
                     <h2>Introducing The Kahoot! League</h2>
                     <p>The Kahoot! League makes it cooler than ever for students to show off their knowledge. By using The Kahoot! League and collecting points, students climb the leaderboards and establish themselves as class all-stars. Students learn faster, advance further, and keep getting better all semester long.</p>
@@ -32,7 +32,7 @@ function Home(props) {
 
             <article id='introduction-2'>
 
-                <img id="image2" src={Sky} alt="phone" loading="lazy" />
+                {/* <img id="image2" src={Sky} alt="phone" loading="lazy" /> */}
                 <div id="text2">
                     <h2>Visualize your quiz scores</h2>
                     <p>We know that taking quizzes in class can feel daunting. Sometimes you rush through a quiz because of time constraints, but other times a tough question can really stress you out. We have created this website to help you visualize your statistics</p>
@@ -42,7 +42,7 @@ function Home(props) {
 
             <article id='introduction-3'>
 
-                <img id="image3" src={Phone} alt="phone" loading="lazy" />
+                {/* <img id="image3" src={Phone} alt="phone" loading="lazy" /> */}
                 <div id="text3">
                     <h2>Introducing The Kahoot! League</h2>
                     <p>The Kahoot! League makes it cooler than ever for students to show off their knowledge. By using The Kahoot! League and collecting points, students climb the leaderboards and establish themselves as class all-stars. Students learn faster, advance further, and keep getting better all semester long.</p>
