3a35840fcfa9fadbe32dddaf102ef5ca202f72c4
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-16 17:13:14 +0100

    fixed interceptors

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,47 +1,85 @@
-import axios from "axios";
-import jwt_decode from 'jwt-decode';
-import { checkDiff } from "../helpers/functions";
+import axios from 'axios';
+//import jwt_decode from 'jwt-decode';
+//import { checkDiff } from "../helpers/functions";
 
-let authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null;
+//console.log("This is the API ", process.env);
 
-let baseURL;
-let axiosInstance;
+let axiosInstance = axios;
 if (process.env && process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
-    baseURL = process.env.REACT_APP_BACKEND;
     axiosInstance = axios.create({
         withCredentials: true,
-        baseURL,
-        headers: { Authorization: `Bearer ${authTokens?.jwtToken}` }
+        baseURL: process.env.REACT_APP_BACKEND
     });
 } else {
-    baseURL = process.env.REACT_APP_BACKEND_LOCAL;
-    axiosInstance = axios.create({
-        baseURL,
-        headers: { Authorization: `Bearer ${authTokens?.jwtToken}` }
-    });
+    axiosInstance = axios.create();
 }
 
-axiosInstance.interceptors.request.use(async req => {
-    if (!authTokens) {
-        authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null;
-        req.headers.Authorization = `Bearer ${authTokens?.jwtToken}`;
-    }
+function createAxiosResponseInterceptor(axiosInstance) {
+    let authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null;
+    axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + authTokens?.jwtToken;
 
-    authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null;
-    const user = jwt_decode(authTokens?.jwtToken);
-    const isExpired = checkDiff(user.exp);
+    const refreshToken = async () => {
+        const response = await axios.post(`/api/refresh/`);;
 
-    if (!isExpired) return req;
-    try {
-        const response = await axios.post(`/api/refresh/`);
         localStorage.setItem('authTokens', JSON.stringify(response.data));
-        req.headers.Authorization = `Bearer ${response.data.jwtToken}`;
-        return req;
-    } catch (error) {
-        return window.location.href = '/logout';
+
+        axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${response.data.jwtToken}`;
     }
 
+    axiosInstance.interceptors.request.use(
+        function (config) {
+            let authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null;
+            const token = authTokens?.jwtToken;
+            if (token) {
+                config.headers['Authorization'] = 'Bearer ' + token;
+            }
+            //console.log(`${config.method.toUpperCase()} Request made to ${config.url} with data:`, config.data);
+            return config
+        },
+        function (err) {
+            //console.log(err);
+            return err;
+        }
+    );
 
-})
+    axiosInstance.interceptors.response.use(
+        function (response) {
+            /* const { status, data, config } = response;
+            console.log(`Response from ${config.url}:`, {
+                code: status,
+                ...data
+            }); */
+            return response;
+        },
+        async function (error) {
+            if (error.response) {
+                const { status, data } = error.response;
+
+                switch (status) {
+                    case 401:
+                        if (data === 'Unauthorized') {
+                            try {
+                                await refreshToken();
+                                const config = error.config;
+                                return await axiosInstance({ method: config.method, url: config.url, data: config.data });
+                            } catch (e) {
+                                return window.location.href = '/logout';
+                            }
+                        } else {
+                            return window.location.href = '/logout';
+                        }
+                    default:
+                        return Promise.reject(error);
+                }
+            } else if (error.request) {
+                return Promise.reject(error);
+            } else {
+                return Promise.reject(error);
+            }
+        }
+    )
+}
+
+createAxiosResponseInterceptor(axiosInstance)
 
 export default axiosInstance;
