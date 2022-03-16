bb552dc21a3a91d78458178935952e5afb462853
Tom <54577949+TomSchrier@users.noreply.github.com>
2022-03-16 18:45:21 +0100

    css fixes anf display file size on uploading quiz

--- src/components/PopUp/UploadQuiz/UploadQuiz.jsx
+++ src/components/PopUp/UploadQuiz/UploadQuiz.jsx
@@ -7,7 +7,7 @@ function UploadQuiz({ modalTitle, bodyText, handleClose, uploadQuiz }) {
     const [selectedFile, setSelectedFile] = useState(null);
 
     const courses = ['IDG2100', 'idg0002', 'idg0003', 'idg0004']; /* ← API fra backend */
-    const semesters = ['h2019', 'v2020', 'h2020', 'v2021', 'h2021', 'v2022']; /* ← API fra backend */
+    const semesters = ['f2019', 's2020', 'f2020', 's2021', 'f2021', 's2022']; /* ← API fra backend */
 
     const [selectedCourse, setSelectedCourse] = useState(courses[0]);  /* ← Sette inn mest nylig course i useSate() for eksempel useSate(IDG2100)*/
     const [selectedSemester, setSelectedSemester] = useState(semesters[0]); /* ← Sette inn current semester i useSate() for eksempel useSate(f2021)*/
@@ -26,9 +26,28 @@ function UploadQuiz({ modalTitle, bodyText, handleClose, uploadQuiz }) {
         setSelectedFile(e.target.files[0]);
     }
 
+    const handleSelectedCourse = (e) => {
+        setSelectedCourse(e.target.value);
+    }
+
+    const handleSelectedSemester = (e) => {
+        setSelectedSemester(e.target.value);
+    }
+
+    const removeSelectedFile = () => {
+        formData.delete('file');
+        setSelectedFile(null);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.info(formData.get('text'))
+
+        if (!selectedFile) {
+            console.error('No file selected upon submit!');
+            return
+        }
+
+        console.info(formData.get('file'))
 
         uploadQuiz(formData);
     }
@@ -49,11 +68,11 @@ function UploadQuiz({ modalTitle, bodyText, handleClose, uploadQuiz }) {
                     accept=".xls,.xlsx"
                     required
                 />
-                <p>{selectedFile ? `Selected file: ${selectedFile.name}` : 'File must be .xls or .xlsx'}</p>
+                <p>{selectedFile ? <>Selected file: {selectedFile.name} ({Math.round(selectedFile.size/1000)}kb) <span onMouseDown={removeSelectedFile}>(remove)</span></> : 'File must be .xls or .xlsx'}</p>
 
                 <label className='required' htmlFor='course-code-selector'>Choose corresponding course code</label>
 
-                <select value={selectedCourse} id='course-code-selector' onChange={e => setSelectedCourse(e.target.value)}>
+                <select value={selectedCourse} className='custom-select' id='course-code-selector' onChange={handleSelectedCourse}>
 
                     {/*  ↓ map liste med eksisterende emner her (bytt ut arrayet) ↓*/}
                     {courses.map(course =>
@@ -62,7 +81,7 @@ function UploadQuiz({ modalTitle, bodyText, handleClose, uploadQuiz }) {
                 </select>
 
                 <label className='required' htmlFor='semester-selector'>Choose corresponding semester</label>
-                <select value={selectedSemester} id='semester-selector' onChange={e => setSelectedSemester(e.target.value)}>
+                <select value={selectedSemester} className='custom-select' id='semester-selector' onChange={handleSelectedSemester}>
 
                     {/*  ↓ map liste med eksisterende semestere her. (bytt ut arrayet) ↓ */}
                     {semesters.map(semester =>
