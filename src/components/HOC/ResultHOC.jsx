ef16fdf94005df4145da8fe19906d9eee64d5ec2
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-17 15:19:14 +0100

    sessionStorage

--- src/components/HOC/ResultHOC.jsx
+++ src/components/HOC/ResultHOC.jsx
@@ -15,71 +15,97 @@ function resultHoc(WrappedComponent) {
             }
         }
 
-        fetchHome = async() => {
-            const res = await fetchHome();
-            if(res.error) {
+        fetchHome = async () => {
+            if (sessionStorage.getItem('fetchHomeData')) {
                 this.setState({
-                    error: res.error,
-                    loading: false
+                    loading: false,
+                    fetchHomeData: JSON.parse(sessionStorage.getItem('fetchHomeData'))
                 })
             } else {
-                this.setState({
-                    fetchHomeData: res.data.studyProgrammeData,
-                    loading: false
-                });
+                const res = await fetchHome();
+                if (res.error) {
+                    this.setState({
+                        error: res.error,
+                        loading: false
+                    })
+                } else {
+                    sessionStorage.setItem('fetchHomeData', JSON.stringify(res.data.studyProgrammeData));
+                    this.setState({
+                        fetchHomeData: res.data.studyProgrammeData,
+                        loading: false
+                    });
+                }
             }
+
         }
 
         fetchCourseTable = async (courseId) => {
-            const res = await fetchCourse(courseId);
-            //console.log(res.data);
-            if (res.error) {
-                this.setState({
-                    error: res.error,
-                    loading: false
-                });
-            } else {
+            if (sessionStorage.getItem('fetchCourseTableData') && sessionStorage.getItem('fetchCourseData')) {
                 this.setState({
                     loading: false,
-                    fetchCourseTableData: res.data.studyProgrammeData,
-                    fetchCourseData: res.data.getUserSpecific
+                    fetchCourseTableData: JSON.parse(sessionStorage.getItem('fetchCourseTableData')),
+                    fetchCourseData: JSON.parse(sessionStorage.getItem('fetchCourseData'))
                 })
+            } else {
+                const res = await fetchCourse(courseId);
+                if (res.error) {
+                    this.setState({
+                        error: res.error,
+                        loading: false
+                    });
+                } else {
+                    sessionStorage.setItem('fetchCourseTableData', JSON.stringify(res.data.studyProgrammeData));
+                    sessionStorage.setItem('fetchCourseData', JSON.stringify(res.data.getUserSpecific))
+                    this.setState({
+                        loading: false,
+                        fetchCourseTableData: res.data.studyProgrammeData,
+                        fetchCourseData: res.data.getUserSpecific
+                    })
+                }
             }
+
         }
 
         fetchQuiz = async (courseId, quizId) => {
-            const res = await fetchQuiz(courseId, quizId);
-            console.log(res);
-            if (res.error) {
-                this.setState({
-                    error: res.error,
-                    loading: false
-                });
-            } else {
+            if (sessionStorage.getItem(courseId)) {
                 this.setState({
                     loading: false,
-                    fetchQuizData: res.data.getUserSpecific[0]
+                    fetchQuizData: JSON.parse(sessionStorage.getItem(quizId))
                 });
+            } else {
+                const res = await fetchQuiz(courseId, quizId);
+                if (res.error) {
+                    this.setState({
+                        error: res.error,
+                        loading: false
+                    });
+                } else {
+                    sessionStorage.setItem(quizId, JSON.stringify(res.data.getUserSpecific[0]));
+                    this.setState({
+                        loading: false,
+                        fetchQuizData: res.data.getUserSpecific[0]
+                    });
+                }
             }
         }
 
-        render() { 
+        render() {
             return (
-                <WrappedComponent 
-                loading={this.state.loading} 
-                fetchCourseTable={this.fetchCourseTable} 
-                fetchCourseTableData={this.state.fetchCourseTableData}
-                fetchCourseData={this.state.fetchCourseData}
-                fetchHomeData={this.state.fetchHomeData} 
-                fetchHome={this.fetchHome} 
-                fetchQuizData={this.state.fetchQuizData}
-                fetchQuiz={this.fetchQuiz}
-                error={this.state.error} 
-                {...this.props} />
+                <WrappedComponent
+                    loading={this.state.loading}
+                    fetchCourseTable={this.fetchCourseTable}
+                    fetchCourseTableData={this.state.fetchCourseTableData}
+                    fetchCourseData={this.state.fetchCourseData}
+                    fetchHomeData={this.state.fetchHomeData}
+                    fetchHome={this.fetchHome}
+                    fetchQuizData={this.state.fetchQuizData}
+                    fetchQuiz={this.fetchQuiz}
+                    error={this.state.error}
+                    {...this.props} />
             );
         }
     }
-    
+
     return ResultsHoc;
 }
 
