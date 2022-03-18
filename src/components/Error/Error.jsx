900ee2839c13982cb935bf501be78de22d641c67
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-18 15:34:49 +0100

    Add go back button to error component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import Icon from '../Icon/Icon';
 import './error.css';
 
 function Error() {
+    const history = useNavigate();
+
     return (
         <section className='error'>
             <h1>The Kahoot! League</h1>
@@ -14,7 +16,8 @@ function Error() {
                     Something went wrong. Please try again or refresh the page!
                 </p>
             </div>
-            <Link to='/'><Button label='Return Home' /></Link>
+            <Button label='Go Back' onClick={() => history(-1)} />
+            <Button label='Return Home' variant='secondary' onClick={() => history('/')} />
         </section>
     );
 }
