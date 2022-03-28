dcca438d467a7e21cd16bf6c7c6606e266eb2eaa
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-28 13:25:04 +0200

    Add close popup with Esc

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -11,17 +11,26 @@ function PopUp(props) {
             }
         }
 
+        function keyPress(event) {
+            const key = event.key;
+            if (key === 'Escape') {
+                props.handleClose(props.type);
+            }
+        }
+
         let scrollPosition = window.pageYOffset;
         document.body.style.overflow = 'hidden'; // Er mulig all koden under er redundant, hidden fikset scrolling
         document.body.style.top = `-${scrollPosition}px`;
         document.body.style.width = '100%';
-        document.addEventListener("click", handleOutsideClick);
+        document.addEventListener('click', handleOutsideClick);
+        document.addEventListener('keydown', keyPress);
         return () => {
             document.body.style.removeProperty('overflow');
             document.body.style.removeProperty('top');
             document.body.style.removeProperty('width');
             window.scrollTo(0, scrollPosition);
-            document.removeEventListener("click", handleOutsideClick);
+            document.removeEventListener('click', handleOutsideClick);
+            document.removeEventListener('keydown', keyPress);
         }
     }, [wrapperRef, props]);
 
